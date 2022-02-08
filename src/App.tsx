994a39431470f823966b4d457d43f5aec5ce8a56
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-02-08 21:44:20 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Topnav from "./components/Topnav";
+import Topnav from "./components/homapage/Topnav";
 
 function App() {
   return (
