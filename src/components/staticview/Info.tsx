1b7973b38d93a7722608e874f08aa1d945e3c013
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-02-11 13:52:49 +0100

    .

---
+++ src/components/staticview/Info.tsx
@@ -0,0 +1,12 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Info = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>hello</Col>
+      </Row>
+    </Container>
+  );
+};
+export default Info;
---
+++ src/utils/consts/assets.ts
@@ -0,0 +1,2 @@
+export const LOGO = "../../../assets/logo.svg";
+export const pLOGO = "../../../assets/logo.png";
