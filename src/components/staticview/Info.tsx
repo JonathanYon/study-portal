79d52a8d97b1d40f7607e37b16fca088ef028ece
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-02-12 20:47:25 +0100

    info update

--- src/components/staticview/Info.tsx
+++ src/components/staticview/Info.tsx
@@ -1,43 +1,74 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { elearn, estudy } from "utils/consts/assets";
+import { elearn, shapPic, typingPic, shapePic } from "utils/consts/assets";
 import "./info.css";
 
 const Info = () => {
   return (
     <Container fluid>
-      <Row className="d-flex">
-        <Col
+      <Row className="d-flex position-relative">
+        <div
           style={{
-            backgroundImage: `url(${estudy})`,
-            backgroundRepeat: "no-repeat",
-            objectFit: "cover",
-            height: "100vh",
+            transform: "skew(0deg, 0deg)",
+            width: "115%",
+            position: "absolute",
+            overflow: "hidden",
+            top: "0%",
+            left: "-21%",
+            height: "110%",
+            borderBottomRightRadius: "30rem",
+            zIndex: "2",
+            backgroundColor: "#f4f5f7",
+            transformStyle: "preserve-3d",
           }}
         >
-          hello
-        </Col>
-        <Col
+          <Col
+            style={{
+              backgroundImage: `url(${typingPic})`,
+              backgroundRepeat: "no-repeat",
+              objectFit: "cover",
+              height: "89vh",
+              width: "112vw",
+              top: "0%",
+              left: "5%",
+              // zIndex: "2",
+              // position: "absolute",
+            }}
+          >
+            hello
+          </Col>
+        </div>
+        <div
           style={{
-            backgroundImage: `url(${elearn})`,
-            backgroundRepeat: "no-repeat",
-            objectFit: "cover",
-            height: "100vh",
-            backgroundSize: "100% 75%",
-            marginLeft: "0.2rem",
+            transform: "skew(0deg, 0deg)",
+            width: "100%",
+            // position: "absolute",
+            overflow: "hidden",
+            top: "0%",
+            right: "-21%",
+            height: "110%",
+            borderBottomLeftRadius: "40rem",
+            // zIndex: "-2",
+            backgroundColor: "#1b69b3",
+            transformStyle: "preserve-3d",
           }}
         >
-          hel
-        </Col>
+          <Col
+            style={{
+              backgroundImage: `url(${shapPic})`,
+              backgroundRepeat: "no-repeat",
+              objectFit: "cover",
+              height: "90vh",
+              marginLeft: "40rem",
+              backgroundSize: "100% 100%",
+
+              zIndex: "3",
+            }}
+          >
+            held
+          </Col>
+        </div>
       </Row>
     </Container>
   );
 };
 export default Info;
-
-/* <div
-            style={{
-              backgroundImage: `url(${rightImage})`,
-              height: "90vh",
-              objectFit: "cover",
-            }}
-          ></div> */
