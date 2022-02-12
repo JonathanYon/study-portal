edc0d9f598a168053439b7ca22b0b464ad8f174b
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-02-12 20:47:04 +0100

    .

--- src/utils/consts/assets.ts
+++ src/utils/consts/assets.ts
@@ -2,3 +2,6 @@ export const LOGO = "../../../assets/logo.svg";
 export const pLOGO = "../../../assets/logo.png";
 export const elearn = "../../../assets/e-study.png";
 export const estudy = "../../../assets/elearning.jpg";
+export const shapePic = "../../../assets/shape.png";
+export const shapPic = "../../../assets/shap.png";
+export const typingPic = "../../../assets/typing.jpg";
