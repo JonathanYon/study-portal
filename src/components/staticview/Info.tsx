a1a7e53470dbbd713dd2a6a4306520de100ef8f6
Yonanni <adhanamengisteab@gmail.com>
2022-02-12 14:58:39 +0100

    static info

--- src/components/staticview/Info.tsx
+++ src/components/staticview/Info.tsx
@@ -1,10 +1,33 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { elearn, estudy } from "utils/consts/assets";
+import "./info.css";
 
 const Info = () => {
   return (
-    <Container>
-      <Row>
-        <Col>hello</Col>
+    <Container fluid>
+      <Row className="d-flex">
+        <Col
+          style={{
+            backgroundImage: `url(${estudy})`,
+            backgroundRepeat: "no-repeat",
+            objectFit: "cover",
+            height: "100vh"
+          }}
+        >
+          hello
+        </Col>
+        <Col
+          style={{
+            backgroundImage: `url(${elearn})`,
+            backgroundRepeat: "no-repeat",
+            objectFit: "cover",
+            height: "100vh",
+            backgroundSize: "100% 75%",
+            // marginLeft: "10rem"
+          }}
+        >
+          hel
+        </Col>
       </Row>
     </Container>
   );
